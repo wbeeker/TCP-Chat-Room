337f39aa9945fb1c71c945a5fe59c101d16c4d17
wbeeker <willbeeker@gmail.com>
2025-04-28 09:49:14 -0700

    Built broadcast, handle, receive server functions.

--- server.py
+++ server.py
@@ -12,13 +12,45 @@ server.listen()
 clients = []
 nicknames = []
 
+
 def broadcast(message):
     for client in clients:
         client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[client.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
 
 def receive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}!")
 
-        client.send()
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} has connected to the server.\n".encode('utf-8'))
+        client.send("Connected to the server".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server running...")
+receive()
