a8fab5d974e73f306022d11f7ef5ce502317e92a
wbeeker <willbeeker@gmail.com>
2025-04-28 09:31:52 -0700

    Add server file

---
+++ server.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        client.send()
